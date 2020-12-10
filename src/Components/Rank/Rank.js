21d30eff6fad19640d8382ecaeb926d2cf19d9af
Joshua Dinh <joshuahungdinh@gmail.com>
2020-12-10 08:27:37 -0800

    Added files

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+    <div style={{display:'flex', justifyContent: 'center'}}className='f3'> 
+    <div className='white f3'>
+          {'Josh, your current rank is..'}
+       <div style={{display:'flex', justifyContent: 'center'}}className='f3'className='white f1'>
+          {'#5'}
+       </div>
+    </div>
+   </div>
+    );
+}
+
+export default Rank;
